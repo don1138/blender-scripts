6c80e515829810fbc8e88be8b02f24a4cadfa6b8
Don Schniitzius <don@sketchbookclub.com>
2022-12-07 15:15:03 -0600

    Correct filepath

--- render.py
+++ render.py
@@ -4,5 +4,13 @@ import bpy
 for step in range(1, 91):
 
     bpy.context.scene.frame_set(step)
-    bpy.data.scene.render.filepath = 'PATH_NAME/frame_%d.png' % step
+
+    # Render the current open scene
+    bpy.context.scene.render.filepath = 'PATH_NAME/frame_%d.png' % step
+
+    # Choose which scene to render
+    # bpy.data.scenes["Scene"].render.filepath = 'PATH_NAME/frame_%d.png' % step
+
     bpy.ops.render.render(write_still=True)
+
+print("Done")
